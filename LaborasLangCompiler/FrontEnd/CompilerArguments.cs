af3025985d1ffee4518172c5b3ed03a45a9e1871
Sunius <suniusx@gmail.com>
2014-03-03 19:53:18 +0200

    Add command line argument parser.

---
+++ LaborasLangCompiler/FrontEnd/CompilerArguments.cs
@@ -0,0 +1,62 @@
+﻿using LaborasLangCompiler.Misc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.FrontEnd
+{
+    internal class CompilerArguments
+    {
+        public readonly string[] SourceFiles;
+        public readonly string[] References;
+
+        public static CompilerArguments Parse(string[] args)
+        {
+            var sourceFiles = args.Where(x => !x.StartsWith("/"));
+            var references = args.Where(x => x.StartsWith("/ref:"));
+
+            var unknownOptions = args.Except(sourceFiles.Union(references));
+
+            if (unknownOptions.Count() > 0)
+            {
+                var message = new StringBuilder();
+
+                foreach (var option in unknownOptions)
+                {
+                    message.AppendLine(string.Format("Unknown compiler switch: {0}.", option));
+                }
+
+                throw new ArgumentException(message.ToString());
+            }
+
+            return new CompilerArguments(sourceFiles, references.Select(x => x.Substring(5)).Union(GetDefaultReferences()));
+        }
+
+        private static IEnumerable<string> GetDefaultReferences()
+        {
+            var referenceAssembliesPath = Utils.CombinePaths(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), 
+                "Reference Assemblies", "Microsoft", "Framework", ".NETFramework", "v4.5");
+
+            var references = new List<string>
+            {
+                Path.Combine(referenceAssembliesPath, "mscorlib.dll")
+            };
+
+            return references;
+        }
+
+        private CompilerArguments(IEnumerable<string> sourceFiles, IEnumerable<string> references)
+        {
+            SourceFiles = sourceFiles.ToArray();
+            References = references.ToArray();
+
+            if (SourceFiles.Length == 0)
+            {
+                throw new ArgumentException("No source files found to compile!");
+            }
+        }
+    }
+}
---
+++ LaborasLangCompiler/FrontEnd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.FrontEnd
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                var compilerArgs = CompilerArguments.Parse(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Compilation failed. Aborting.");
+
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
