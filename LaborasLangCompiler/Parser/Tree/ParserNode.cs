10c72fe8c9eee3c0fa7655b9a626bda6ce523a07
DariusL <d.lapunas@gmail.com>
2014-03-31 22:57:47 +0300

    ParserNode

---
+++ LaborasLangCompiler/Parser/Tree/ParserNode.cs
@@ -0,0 +1,28 @@
+﻿using LaborasLangCompiler.Parser.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.Parser.Tree
+{
+    abstract class ParserNode
+    {
+        public readonly NodeType NodeType;
+        private readonly ParserNode parent;
+    }
+
+    enum NodeType
+    {
+        TypeLiteral,
+        TypeVariable,
+        TypeBinaryOperator,
+        TypeUnaryOperator,
+        TypeAssignmentOperator,
+        TypeFunctionCall,
+        TypeSymbolDeclaration,
+        TypeFunction,
+        TypeFunctionArg//deklaracijos arg, ne call
+    }
+}
