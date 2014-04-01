b18ab69c666bb7a36e8ea7955e46cd9d6a7b853e
AndriusDap <Dapsevicius.Andrius@gmail.com>
2014-04-01 14:21:45 +0300

    Lexer unit test project.
    Need to fix missing method exception.

---
+++ LexerUnitTests/ExpectedTree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NPEG;
+
+namespace LexerUnitTests
+{
+    class ExpectedTree
+    {
+        public List<ExpectedTree> Children;
+        public String Token;
+
+        public ExpectedTree(String token, params string[] children)
+        {
+            Token = token;
+            Children = new List<ExpectedTree>();
+            foreach(var child in children)
+            {
+                Children.Add(new ExpectedTree(child));
+            }
+        }
+
+        public ExpectedTree(string token, params ExpectedTree[] children)
+        {
+            Token = token;
+            foreach (var child in children)
+            {
+                Children.Add(child);
+            }
+        }
+
+        public ExpectedTree(string token)
+        {
+            Token = token;
+            Children = null;
+        }
+
+        public void AsertEqual(AstNode actualTree)
+        {
+            Assert.AreEqual(Token, actualTree.Token);
+            if (Children != null)
+            {
+                Assert.AreEqual(Children.Count, actualTree.Children.Count);
+                for (int i = 0; i < Children.Count; i++)
+                {
+                    Children[i].AsertEqual(actualTree.Children[i]);
+                }
+            }
+        }
+    }
+}
