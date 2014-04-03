b6e10d41103f55551ad3e417376899bf30f91369
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2014-04-03 09:47:39 +0300

    Removed period node

--- LaborasLangCompiler/LexingTools/Lexer.cs
+++ LaborasLangCompiler/LexingTools/Lexer.cs
@@ -16,7 +16,7 @@ namespace LaborasLangCompiler.LexingTools
  
                 Ws: [\n\r\t\s]+;       
                 (?<Symbol>): [a-zA-Z_]+ [a-zA-Z0-9_]*;
-                (?<Period>): [.];
+                Period: [.];
                 (?<FullSymbol>): Symbol (Period Symbol)*;
                 (?<NamespaceImport>): 'use' Ws FullSymbol;
                 
