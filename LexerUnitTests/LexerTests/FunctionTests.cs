8e16e20af64b3a7d1a962428c03741efec074dad
Sunius <suniusx@gmail.com>
2014-04-04 10:29:03 +0300

    Add more tests.

---
+++ LexerUnitTests/LexerTests/FunctionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NPEG;
+using LaborasLangCompiler.LexingTools;
+
+namespace LaborasLangCompilerUnitTests.LexerTests
+{
+    [TestClass]
+    public class FunctionTests
+    {
+        [TestMethod]
+        public void DeclareFunctionTest()
+        {
+            var source = @"int(int, bool) foo;";
+            AstNode tree = Lexer.MakeTree(source);
+
+            Assert.IsNotNull(tree);
+            string expected = "Root: Sentence: (Declaration: (FunctionType: (Type: Symbol, ArgumentTypes: (Type: Symbol, Type: Symbol)), Symbol), EndOfSentence)";
+            string actual = AstHelper.Stringify(tree);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
