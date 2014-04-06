997d506517bcceb3cbc55dd358b06479f5aa5a72
DariusL <d.lapunas@gmail.com>
2014-04-06 22:35:14 +0300

    Added CodeBlockNode, ParserNode

---
+++ LaborasLangCompiler/Parser/Impl/ParserNode.cs
@@ -0,0 +1,26 @@
+﻿using LaborasLangCompiler.Parser.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.Parser.Impl
+{
+    abstract class ParserNode : IParserNode
+    {
+        public abstract NodeType Type { get; }
+        public CodeBlockNode ParentBlock { get; private set; }
+        public virtual ILValueNode GetSymbol(string name)
+        {
+            if (ParentBlock != null)
+                return ParentBlock.GetSymbol(name);
+            else
+                return null;
+        }
+        public ParserNode(CodeBlockNode parent)
+        {
+            ParentBlock = parent;
+        }
+    }
+}
