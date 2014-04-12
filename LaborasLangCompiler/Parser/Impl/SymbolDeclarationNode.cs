d39e02a6bcf7415f523aa3b1112e94de4505cfbe
DariusL <d.lapunas@gmail.com>
2014-04-12 20:13:30 +0300

    Probably implemented SymbolDeclarationNode

--- LaborasLangCompiler/Parser/Impl/SymbolDeclarationNode.cs
+++ LaborasLangCompiler/Parser/Impl/SymbolDeclarationNode.cs
@@ -21,18 +21,47 @@ namespace LaborasLangCompiler.Parser.Impl
         }
         public static new SymbolDeclarationNode Parse(Parser parser, CodeBlockNode parent, AstNode lexerNode)
         {
-            if(lexerNode.Token.Name == "Declaration")
+            ILValueNode symbol = null;
+            IExpressionNode initializer = null;
+            string type = lexerNode.Token.Name;
+            if (type == "Declaration" || type == "DeclarationAndAssignment")
             {
-                throw new NotImplementedException();
-            }
-            else if(lexerNode.Token.Name == "DeclarationAndAssignment")
-            {
-                throw new NotImplementedException();
+                try
+                {
+                    var nodes = parser.FindChildren(new string[] { "Type", "FunctionType", "Symbol", "Function", "Value" }, lexerNode);
+                    string name = parser.GetNodeValue(nodes["Symbol"][0]);
+                    if(nodes.ContainsKey("Type"))
+                    {
+                        symbol = parent.AddSymbol(parser.ParseType(nodes["Type"][0]), name);
+                    }
+                    else if (nodes.ContainsKey("FunctionType"))
+                    {
+                        symbol = parent.AddSymbol(parser.ParseType(nodes["FunctionType"][0]), name);
+                    }
+                    else
+                    {
+                        throw new ParseException("Type not defined in declaration");
+                    }
+
+                    AstNode init = null;
+                    if (nodes.ContainsKey("Value"))
+                        init = nodes["Value"][0];
+                    else if (nodes.ContainsKey("Function"))
+                        init = nodes["Function"][0];
+
+                    if (init != null)
+                        initializer = ExpressionNode.Parse(parser, parent, init);
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    throw new ParseException("Less than one subnode in type/functiontype/value/function", e);
+                }
             }
             else
             {
                 throw new ParseException("Declaration expected, " + lexerNode.Token.Name + " received");
             }
+            return new SymbolDeclarationNode(symbol, initializer);
         }
     }
 }
--- LaborasLangCompiler/Parser/Parser.cs
+++ LaborasLangCompiler/Parser/Parser.cs
@@ -22,10 +22,45 @@ namespace LaborasLangCompiler.Parser
             Registry = registry;
             this.source = source;
             ParserNode.Parse(this, null, tree);
+            primitives = new Dictionary<string, TypeReference>();
+            primitives.Add("bool", Registry.GetType("System.Boolean"));
+            primitives.Add("int", Registry.GetType("System.Int32"));
+            primitives.Add("float", Registry.GetType("System.Single"));
         }
         public string GetNodeValue(AstNode node)
         {
             return Encoding.UTF8.GetString(source.Text(node.Token.Start, node.Token.End));
+        }
+        public Dictionary<string, List<AstNode>> FindChildren(string[] types, AstNode node)
+        {
+            Dictionary<string, List<AstNode>> ret = new Dictionary<string,List<AstNode>>();
+            foreach(var type in types)
+            {
+                ret.Add(type, new List<AstNode>());
+            }
+            foreach(var child in node.Children)
+            {
+                if(ret.ContainsKey(child.Token.Name))
+                {
+                    ret[child.Token.Name].Add(child);
+                }
+            }
+            return ret;
+        }
+        public TypeReference ParseType(AstNode typeNode)
+        {
+            if(typeNode.Children.Count == 1)
+            {
+                string type = GetNodeValue(typeNode.Children[0]);
+                if (primitives.ContainsKey(type))
+                    return primitives[type];
+                else
+                    return null;
+            }
+            else
+            {
+                throw new NotImplementedException("Only parsing primitives");
+            }
         }
     }
 }
