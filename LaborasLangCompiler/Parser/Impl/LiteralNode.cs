a2e0753d04169490431ee52c7969d606e1834d8c
DariusL <d.lapunas@gmail.com>
2014-04-13 19:07:42 +0300

    Better literal parsing

--- LaborasLangCompiler/Parser/Impl/LiteralNode.cs
+++ LaborasLangCompiler/Parser/Impl/LiteralNode.cs
@@ -4,6 +4,7 @@ using Mono.Cecil;
 using NPEG;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,16 +26,27 @@ namespace LaborasLangCompiler.Parser.Impl
         {
             string type = lexerNode.Token.Name;
             string value = parser.GetNodeValue(lexerNode);
-            switch (type)
+            try
             {
-                case "StringLiteral":
-                    return new LiteralNode(value, parser.Primitives["string"]);
-                case "IntegerLiteral":
-                    return new LiteralNode(Convert.ToInt32(value), parser.Primitives["int"]);
-                case "FloatLiteral":
-                    return new LiteralNode(Convert.ToSingle(value), parser.Primitives["float"]);
-                default:
-                    throw new ParseException("Literal expected, " + type + " received");
+                switch (type)
+                {
+                    case "StringLiteral":
+                        return new LiteralNode(value, parser.Primitives["string"]);
+                    case "IntegerLiteral":
+                        return new LiteralNode(Convert.ToInt32(value), parser.Primitives["int"]);
+                    case "FloatLiteral":
+                        return new LiteralNode(Convert.ToSingle(value, CultureInfo.InvariantCulture.NumberFormat), parser.Primitives["float"]);
+                    default:
+                        throw new ParseException("Literal expected, " + type + " received");
+                }
+            }
+            catch(FormatException e)
+            {
+                throw new ParseException("Could not parse " + value + " as an " + type, e);
+            }
+            catch(OverflowException e)
+            {
+                throw new ParseException("Could not fit " + value + " in " + type, e);
             }
         }
     }
