ab49473d68538fd63a5d7a22b46c48854116a836
Sunius <suniusx@gmail.com>
2014-04-13 23:31:27 +0300

    Added TreeSerializer to unit test project.

---
+++ LaborasLangCompilerUnitTests/Utilities/TreeSerializer.cs
@@ -0,0 +1,48 @@
+﻿using NPEG;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LaborasLangCompilerUnitTests.Utilities
+{
+    class TreeSerializer
+    {
+        static private DataContractSerializer _serializer;
+        static private DataContractSerializer Serializer
+        {
+            get
+            {
+                if (_serializer == null)
+                {
+                    _serializer = new DataContractSerializer(typeof(AstNode));
+                }
+
+                return _serializer;
+            }
+        }
+
+        static void Serialize(string fileName, AstNode tree)
+        {
+            using (var writer = new XmlTextWriter(fileName, Encoding.UTF8))
+            {
+                Serializer.WriteObject(writer, tree);
+            }
+        }
+
+        static AstNode Deserialize(string fileName)
+        {
+            using (var streamReader = new StreamReader(fileName))
+            {
+                using (var reader = new XmlTextReader(streamReader))
+                {
+                    return (AstNode)Serializer.ReadObject(reader);
+                }
+            }
+        }
+    }
+}
