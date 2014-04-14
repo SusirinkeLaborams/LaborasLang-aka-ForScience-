83bc440a7a3325f8ada8df15cb2a69e7e6ea1301
Sunius <suniusx@gmail.com>
2014-04-14 22:13:01 +0300

    Add ILHelpers class.

---
+++ LaborasLangCompiler/ILTools/ILHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.ILTools
+{
+    static class ILHelpers
+    {
+        static bool IsAssignableTo(this TypeReference left, TypeReference right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
