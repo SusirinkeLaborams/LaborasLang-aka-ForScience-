6777bcae49bcf0ed984a222ea72e5ac6892706e5
Sunius <suniusx@gmail.com>
2014-04-15 23:10:21 +0300

    Added ability to specify initializers for fields and properties.

--- LaborasLangCompiler/ILTools/Types/TypeEmitter.cs
+++ LaborasLangCompiler/ILTools/Types/TypeEmitter.cs
@@ -1,4 +1,5 @@
-﻿using LaborasLangCompiler.Parser;
+﻿using LaborasLangCompiler.ILTools.Methods;
+using LaborasLangCompiler.Parser;
 using Mono.Cecil;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,8 @@ namespace LaborasLangCompiler.ILTools.Types
     {
         const TypeAttributes DefaultTypeAttributes = TypeAttributes.Class | TypeAttributes.Public | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit;
 
+        private ConstructorEmitter instanceConstructor;
+        private ConstructorEmitter staticConstructor;
         protected TypeDefinition typeDefinition;
 
         public ModuleDefinition Module { get { return typeDefinition.Module; } }
@@ -45,31 +48,75 @@ namespace LaborasLangCompiler.ILTools.Types
             {
                 assembly.AddType(typeDefinition);
             }
+
+            instanceConstructor = new ConstructorEmitter(this, false);
+            staticConstructor = new ConstructorEmitter(this, true);
         }
 
         public void AddMethod(MethodDefinition method)
         {
+            CheckForDuplicates(method.Name);
             typeDefinition.Methods.Add(method);
         }
 
-        public void AddField(FieldDefinition field)
+        public void AddField(FieldDefinition field, IExpressionNode initializer)
         {
+            CheckForDuplicates(field.Name);
             typeDefinition.Fields.Add(field);
+
+            if (initializer != null)
+            {
+                if (field.IsStatic)
+                {
+                    staticConstructor.AddFieldInitializer(field, initializer);
+                }
+                else
+                {
+                    instanceConstructor.AddFieldInitializer(field, initializer);
+                }
+            }
         }
 
-        public void AddProperty(PropertyDefinition property)
+        public void AddProperty(PropertyDefinition property, IExpressionNode initializer)
         {
+            if (property.SetMethod == null && property.GetMethod == null)
+            {
+                throw new ArgumentException("Property has neither a setter nor a getter!", "property");
+            }
+
+            CheckForDuplicates(property.Name);
             typeDefinition.Properties.Add(property);
+
+            if (initializer != null)
+            {
+                bool isStatic = (property.SetMethod != null && property.SetMethod.IsStatic) || 
+                    (property.GetMethod != null && property.GetMethod.IsStatic);
+
+                if (isStatic)
+                {
+                    staticConstructor.AddPropertyInitializer(property, initializer);
+                }
+                else
+                {
+                    instanceConstructor.AddPropertyInitializer(property, initializer);
+                }
+            }
         }
 
-        public void AddInitializer(FieldDefinition field, IExpressionNode initializer)
+        private void CheckForDuplicates(string name)
         {
-            throw new NotImplementedException();
-        }
-
-        public void AddInitializer(PropertyDefinition property, IExpressionNode initializer)
-        {
-            throw new NotImplementedException();
+            if (typeDefinition.Methods.Any(x => x.Name == name))
+            {
+                throw new InvalidOperationException(string.Format("A method with same name already exists in type {0}.", typeDefinition.FullName));
+            }
+            else if (typeDefinition.Fields.Any(x => x.Name == name))
+            {
+                throw new InvalidOperationException(string.Format("A field with same name already exists in type {0}.", typeDefinition.FullName));
+            }
+            else if (typeDefinition.Methods.Any(x => x.Name == name))
+            {
+                throw new InvalidOperationException(string.Format("A method with same name already exists in type {0}.", typeDefinition.FullName));
+            }
         }
 
         public static string ComputeNameFromReturnAndArgumentTypes(TypeReference returnType, IReadOnlyList<TypeReference> arguments)
