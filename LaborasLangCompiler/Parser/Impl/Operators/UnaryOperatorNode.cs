7b86db4e364ceec5f96cf50878acb44ce708c115
DariusL <d.lapunas@gmail.com>
2014-04-17 14:25:09 +0300

    Added UnaryOperatorNode

---
+++ LaborasLangCompiler/Parser/Impl/Operators/UnaryOperatorNode.cs
@@ -0,0 +1,17 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.Parser.Impl
+{
+    class UnaryOperatorNode : RValueNode, IUnaryOperatorNode
+    {
+        public override RValueNodeType RValueType { get { return RValueNodeType.UnaryOperator; } }
+        public override TypeReference ReturnType  { get; set; }
+        public UnaryOperatorNodeType UnaryOperatorType { get; private set; }
+        public IExpressionNode Operand { get; private set; }
+    }
+}
