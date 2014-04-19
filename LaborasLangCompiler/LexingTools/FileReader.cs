2a21feb6b98746f6d71376947069144d5390d23c
Sunius <suniusx@gmail.com>
2014-04-19 20:45:48 +0300

    Fixed file reading.

--- LaborasLangCompiler/LexingTools/FileReader.cs
+++ LaborasLangCompiler/LexingTools/FileReader.cs
@@ -12,7 +12,10 @@ namespace LaborasLangCompiler.LexingTools
     {
         public static ByteInputIterator Read(string filename)
         {
-            var bytes = System.IO.File.ReadAllBytes(filename);
+            // First 2 bytes of a text file is its encoding.. 
+            // read text instead and then convert to bytes
+            var text = File.ReadAllText(filename);
+            var bytes = Encoding.UTF8.GetBytes(text);
             return new ByteInputIterator(bytes);
         }
     }
