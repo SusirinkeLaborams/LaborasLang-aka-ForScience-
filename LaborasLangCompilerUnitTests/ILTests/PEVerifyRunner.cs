c2d47abb2d9a87aa0887a5acd16aadd1e1923fcb
Sunius <suniusx@gmail.com>
2014-04-19 14:38:39 +0300

    Made PEVerify rules a little bit stricter.

--- LaborasLangCompilerUnitTests/ILTests/PEVerifyRunner.cs
+++ LaborasLangCompilerUnitTests/ILTests/PEVerifyRunner.cs
@@ -15,7 +15,6 @@ namespace LaborasLangCompilerUnitTests.ILTests
     {
         private static uint[] CodesToIgnore = new uint[]
         {
-            0x80131854,     // Expected native int, found method on stack (happens when creating functor)
             0x80131861,     // Expected pointer to function on a stack, found native int (happens in Functor.AsDelegate when constructing a delegate
             0x8013186E,     // Instruction cannot be verified (calli)
             0x801318BD,     // initlocals must be set for verifiable methods with one or more local variables.
@@ -57,12 +56,21 @@ namespace LaborasLangCompilerUnitTests.ILTests
             var process = Process.Start(startInfo);
             process.WaitForExit();
 
+            var output = process.StandardOutput.ReadToEnd();
+
             if (process.ExitCode != 0)
             {
-                var output = string.Join("\r\n\r\n", process.StandardOutput.ReadToEnd(), process.StandardError.ReadToEnd());
+                var lines = output.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
-                throw new Exception(string.Format("\r\n\r\nPEVerify.exe {0}\r\nfailed with error code {1}: \r\n{2}", 
-                    arguments, process.ExitCode, output));
+                // First 2 lines are logos, last one is conclusion
+                var errorLines = lines.Skip(2).Take(lines.Length - 3) // This error happens when using ldftn and passing the native int 
+                    .Where(x => !x.Contains("[expected Native Int] Unexpected type on the stack.")).ToArray(); // to functor ctor
+
+                if (errorLines.Length > 0)
+                {
+                    throw new Exception(string.Format("\r\n\r\nPEVerify.exe {0}\r\nfailed with error code {1}: \r\n{2}",
+                        arguments, process.ExitCode, string.Join("\r\n", errorLines)));
+                }
             }
         }
     }
