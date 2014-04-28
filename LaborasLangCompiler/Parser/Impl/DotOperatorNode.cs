ea5670f75555383a0bdb902a26bc7ca8515bc022
DariusL <d.lapunas@gmail.com>
2014-04-28 22:28:11 +0300

    Added DotOperatorNode

---
+++ LaborasLangCompiler/Parser/Impl/DotOperatorNode.cs
@@ -0,0 +1,34 @@
+﻿using Mono.Cecil;
+using NPEG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.Parser.Impl
+{
+    abstract class DotOperatorNode
+    {
+        public static IExpressionNode Parse(Parser parser, IContainerNode parent, AstNode lexerNode)
+        {
+            throw new NotImplementedException();
+        }
+        public static TypeNode ParseAsType(Parser parser, IContainerNode parent, AstNode lexerNode, ISymbolNode left = null)
+        {
+            throw new NotImplementedException();
+        }
+        public static NamespaceNode ParseAsNamespace(Parser parser, IContainerNode parent, AstNode lexerNode, NamespaceNode left = null)
+        {
+            throw new NotImplementedException();
+        }
+        public static LValueNode ParseAsLValue(Parser parser, IContainerNode parent, AstNode lexerNode, ISymbolNode left = null)
+        {
+            throw new NotImplementedException();
+        }
+        public static IFunctionNode ParseAsMethod(Parser parser, IContainerNode parent, AstNode lexerNode, List<TypeReference> args = null, ISymbolNode left = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
