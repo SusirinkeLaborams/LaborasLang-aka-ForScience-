a2f202d1b5c2c2505be41c749933418515a0cf03
DariusL <d.lapunas@gmail.com>
2014-04-28 22:14:16 +0300

    Added SymbolNode

---
+++ LaborasLangCompiler/Parser/Impl/SymbolNode.cs
@@ -0,0 +1,42 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.Parser.Impl
+{
+    enum SymbolNodeType
+    {
+        Namespace,
+        Type,
+        LValue
+    }
+    interface ISymbolNode
+    {
+        SymbolNodeType SymbolType { get; }
+    }
+    class NamespaceNode : ISymbolNode
+    {
+        public SymbolNodeType SymbolType { get { return SymbolNodeType.Namespace; } }
+        public string FullNamespace { get; private set; }
+        public NamespaceNode(string namespaze)
+        {
+            FullNamespace = namespaze;
+        }
+    }
+    class TypeNode : ISymbolNode
+    {
+        public SymbolNodeType SymbolType { get { return SymbolNodeType.Type; } }
+        public TypeReference ReturnType { get; private set; }
+        public TypeNode(TypeReference type)
+        {
+            ReturnType = type;
+        }
+    }
+    abstract class SymbolNode
+    {
+
+    }
+}
