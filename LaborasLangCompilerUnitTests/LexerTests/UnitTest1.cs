77640374d80a1f6b3ad39f269292de40499dc781
AndriusDap <Dapsevicius.Andrius@gmail.com>
2014-06-27 13:30:20 +0300

    Tokenizer tests

--- LaborasLangCompilerUnitTests/LexerTests/UnitTest1.cs
+++ LaborasLangCompilerUnitTests/LexerTests/UnitTest1.cs
@@ -15,9 +15,9 @@ namespace LaborasLangCompilerUnitTests.LexerTests
     public class TokenizerTests
     {
         private const string Path = @"..\..\LexerTests\Tokens\";
-        private const bool Tokenize = true;
+        private const bool Tokenize = false;
 
-        [TestMethod, TestCategory("Lexer")]
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
         public void TestTokenizer()
         {
             var source =
@@ -26,10 +26,159 @@ namespace LaborasLangCompilerUnitTests.LexerTests
 	System.Console.WriteLine('Hello, world!');
 	System.Console.ReadKey();
 };";
-            AssertUnmodified(source);
+            ExecuteTest(source);
+        }
+        
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerStringLiteral1()
+        {
+            var source = @"'str\'ing'";
+            ExecuteTest(source);
         }
 
-        void AssertUnmodified(string source, [CallerMemberName] string fileName = "")
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerStringLiteral2()
+        {
+            var source = @"""str\""ing""";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerPlus()
+        {
+            var source = @"+ += ++";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerMinus()
+        {
+            var source = @"- -= --";
+            ExecuteTest(source);
+        }        
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerNot()
+        {
+            var source = @"! !=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerBitwiseComplement()
+        {
+            var source = @"~ ~=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerAnd()
+        {
+            var source = @"& && &=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerXor()
+        {
+            var source = @"^ ^=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerOr()
+        {
+            var source = @"| |= ||";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerLessThan()
+        {
+            var source = @"< << <= <<=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerMoreThan()
+        {
+            var source = @"> >> >= >>=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerDivision()
+        {
+            var source = @"/ /= /* content */ //content
+";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerMultiplication()
+        {
+            var source = @"* *=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerRemainder()
+        {
+            var source = @"% %=";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerEqual()
+        {
+            var source = @"= ==";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerCurlyBracket()
+        {
+            var source = @"{ }";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerBracket()
+        {
+            var source = @"( )";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerSemicolon()
+        {
+            var source = @";";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerNumber()
+        {
+            var source =
+@"1 2 3 4 5 6 7 8 9 0 10 10f 10F 10l 10L 10.5f 10.5F 10..5 10.5L";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerDot()
+        {
+            var source = @".";
+            ExecuteTest(source);
+        }
+
+        [TestMethod, TestCategory("Lexer"), TestCategory("Tokenizer"), Timeout(1000)]
+        public void TestTokenizerSymbol()
+        {
+            var source = @"symbol";
+            ExecuteTest(source);
+        }
+
+        void ExecuteTest(string source, [CallerMemberName] string fileName = "")
         {
             fileName = Path + fileName + ".xml";
             IEnumerable<Token> tokens = null;
