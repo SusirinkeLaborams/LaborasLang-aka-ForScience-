6f67a5a6518e61665ec9598672026339c8268b77
AndriusDap <Dapsevicius.Andrius@gmail.com>
2014-07-30 17:31:13 +0300

    Improved syntax rules readability

--- Lexer/ParseRule.cs
+++ Lexer/ParseRule.cs
@@ -17,6 +17,12 @@ namespace Lexer
         public TokenType Token;
         public ConditionType Type;
 
+        public Condition(Condition token, ConditionType type)
+        {
+            Token = token.Token;
+            Type = type;
+        }
+
         public Condition(TokenType token, ConditionType type)
         {
             Token = token;
@@ -27,19 +33,37 @@ namespace Lexer
         {
             return new Condition(token, ConditionType.One);
         }
+
+        public static implicit operator Condition[](Condition token)
+        {
+            return new Condition[] { token };
+        }
         
+        public static Condition[] operator + (Condition a, Condition b)
+        {
+            var array = new Condition[] { a, b };
+            return array;
+        }
+
+        public static Condition[] operator + (Condition[] a, Condition b)
+        {
+            var array = new Condition[a.Length + 1];
+            a.CopyTo(array, 0);
+            array[a.Length] = b;
+            return array;
+        }
     }
     struct ParseRule
     {
         public TokenType Result;
         public IEnumerable<List<Condition>> RequiredTokens;
-        public ParseRule(TokenType result, params List<Condition>[] requiredTokens)
+        public ParseRule(Condition result, params Condition[][] requiredTokens)
         {
-            Result = result;
-            RequiredTokens = requiredTokens;
+            Result = result.Token;
+            RequiredTokens = requiredTokens.Select(x => x.ToList());
         }
 
-        public ParseRule(TokenType result, params IEnumerable<Condition>[] requiredTokens)
+        public ParseRule(TokenType result, params Condition[][] requiredTokens)
         {
             Result = result;
             RequiredTokens = requiredTokens.Select(x => x.ToList());
--- Lexer/SyntaxMatcher.cs
+++ Lexer/SyntaxMatcher.cs
@@ -11,7 +11,123 @@ namespace Lexer
     {
         private Dictionary<TokenType, ParseRule> m_ParseRules;
         private List<Token> m_Source;
-        private IEnumerable<Token> enumerable;
+
+        #region TokenProperties
+        private Condition EndOfLine { get { return TokenType.EndOfLine; } }
+        private Condition Comma { get { return TokenType.Comma; } }
+        private Condition Period { get { return TokenType.Period; } }
+        private Condition Comment { get { return TokenType.Comment; } }
+        private Condition BitwiseAnd { get { return TokenType.BitwiseAnd; } }
+        private Condition BitwiseAndEqual { get { return TokenType.BitwiseAndEqual; } }
+        private Condition And { get { return TokenType.And; } }
+        private Condition Plus { get { return TokenType.Plus; } }
+        private Condition PlusPlus { get { return TokenType.PlusPlus; } }
+        private Condition Minus { get { return TokenType.Minus; } }
+        private Condition MinusMinus { get { return TokenType.MinusMinus; } }
+        private Condition MinusEqual { get { return TokenType.MinusEqual; } }
+        private Condition NotEqual { get { return TokenType.NotEqual; } }
+        private Condition Not { get { return TokenType.Not; } }
+        private Condition Whitespace { get { return TokenType.Whitespace; } }
+        private Condition PlusEqual { get { return TokenType.PlusEqual; } }
+        private Condition StringLiteral { get { return TokenType.StringLiteral; } }
+        private Condition BitwiseComplementEqual { get { return TokenType.BitwiseComplementEqual; } }
+        private Condition BitwiseComplement { get { return TokenType.BitwiseComplement; } }
+        private Condition BitwiseXor { get { return TokenType.BitwiseXor; } }
+        private Condition BitwiseXorEqual { get { return TokenType.BitwiseXorEqual; } }
+        private Condition BitwiseOr { get { return TokenType.BitwiseOr; } }
+        private Condition Or { get { return TokenType.Or; } }
+        private Condition BitwiseOrEqual { get { return TokenType.BitwiseOrEqual; } }
+        private Condition LeftShiftEqual { get { return TokenType.LeftShiftEqual; } }
+        private Condition LeftShift { get { return TokenType.LeftShift; } }
+        private Condition LessOrEqual { get { return TokenType.LessOrEqual; } }
+        private Condition Less { get { return TokenType.Less; } }
+        private Condition More { get { return TokenType.More; } }
+        private Condition RightShift { get { return TokenType.RightShift; } }
+        private Condition RightShiftEqual { get { return TokenType.RightShiftEqual; } }
+        private Condition MoreOrEqual { get { return TokenType.MoreOrEqual; } }
+        private Condition Divide { get { return TokenType.Divide; } }
+        private Condition DivideEqual { get { return TokenType.DivideEqual; } }
+        private Condition Multiply { get { return TokenType.Multiply; } }
+        private Condition MultiplyEqual { get { return TokenType.MultiplyEqual; } }
+        private Condition Remainder { get { return TokenType.Remainder; } }
+        private Condition RemainderEqual { get { return TokenType.RemainderEqual; } }
+        private Condition Assignment { get { return TokenType.Assignment; } }
+        private Condition Equal { get { return TokenType.Equal; } }
+        private Condition LeftCurlyBracket { get { return TokenType.LeftCurlyBracket; } }
+        private Condition RightCurlyBracket { get { return TokenType.RightCurlyBracket; } }
+        private Condition LeftBracket { get { return TokenType.LeftBracket; } }
+        private Condition RightBracket { get { return TokenType.RightBracket; } }
+        private Condition Unknown { get { return TokenType.Unknown; } }
+        private Condition Integer { get { return TokenType.Integer; } }
+        private Condition Float { get { return TokenType.Float; } }
+        private Condition Long { get { return TokenType.Long; } }
+        private Condition Double { get { return TokenType.Double; } }
+        private Condition MalformedToken { get { return TokenType.MalformedToken; } }
+        private Condition Symbol { get { return TokenType.Symbol; } }
+        private Condition Abstract { get { return TokenType.Abstract; } }
+        private Condition As { get { return TokenType.As; } }
+        private Condition Base { get { return TokenType.Base; } }
+        private Condition Break { get { return TokenType.Break; } }
+        private Condition Case { get { return TokenType.Case; } }
+        private Condition Catch { get { return TokenType.Catch; } }
+        private Condition Class { get { return TokenType.Class; } }
+        private Condition Const { get { return TokenType.Const; } }
+        private Condition Continue { get { return TokenType.Continue; } }
+        private Condition Default { get { return TokenType.Default; } }
+        private Condition Do { get { return TokenType.Do; } }
+        private Condition Extern { get { return TokenType.Extern; } }
+        private Condition Else { get { return TokenType.Else; } }
+        private Condition Enum { get { return TokenType.Enum; } }
+        private Condition False { get { return TokenType.False; } }
+        private Condition Finally { get { return TokenType.Finally; } }
+        private Condition For { get { return TokenType.For; } }
+        private Condition Goto { get { return TokenType.Goto; } }
+        private Condition If { get { return TokenType.If; } }
+        private Condition Interface { get { return TokenType.Interface; } }
+        private Condition Internal { get { return TokenType.Internal; } }
+        private Condition Is { get { return TokenType.Is; } }
+        private Condition New { get { return TokenType.New; } }
+        private Condition Null { get { return TokenType.Null; } }
+        private Condition Namespace { get { return TokenType.Namespace; } }
+        private Condition Operator { get { return TokenType.Operator; } }
+        private Condition Out { get { return TokenType.Out; } }
+        private Condition Override { get { return TokenType.Override; } }
+        private Condition Protected { get { return TokenType.Protected; } }
+        private Condition Ref { get { return TokenType.Ref; } }
+        private Condition Return { get { return TokenType.Return; } }
+        private Condition Switch { get { return TokenType.Switch; } }
+        private Condition Sealed { get { return TokenType.Sealed; } }
+        private Condition This { get { return TokenType.This; } }
+        private Condition Throw { get { return TokenType.Throw; } }
+        private Condition Struct { get { return TokenType.Struct; } }
+        private Condition True { get { return TokenType.True; } }
+        private Condition Try { get { return TokenType.Try; } }
+        private Condition Using { get { return TokenType.Using; } }
+        private Condition Virtual { get { return TokenType.Virtual; } }
+        private Condition While { get { return TokenType.While; } }
+        private Condition Static { get { return TokenType.Static; } }
+        private Condition Constant { get { return TokenType.Constant; } }
+        private Condition Private { get { return TokenType.Private; } }
+        private Condition Public { get { return TokenType.Public; } }
+        private Condition NonTerminalToken { get { return TokenType.NonTerminalToken; } }
+        private Condition StatementNode { get { return TokenType.StatementNode; } }
+        private Condition CodeBlockNode { get { return TokenType.CodeBlockNode; } }
+        private Condition DeclarationNode { get { return TokenType.DeclarationNode; } }
+        private Condition AssignmentNode { get { return TokenType.AssignmentNode; } }
+        private Condition RootNode { get { return TokenType.RootNode; } }
+        private Condition FullSymbol { get { return TokenType.FullSymbol; } }
+        private Condition SubSymbol { get { return TokenType.SubSymbol; } }
+        private Condition Value { get { return TokenType.Value; } }
+        private Condition LValue { get { return TokenType.LValue; } }
+        private Condition RValue { get { return TokenType.RValue; } }
+        private Condition Type { get { return TokenType.Type; } }
+        private Condition VariableModifier { get { return TokenType.VariableModifier; } }
+        private Condition FunctionCall { get { return TokenType.FunctionCall; } }
+        private Condition FunctionArgument { get { return TokenType.FunctionArgument; } }
+        private Condition FunctionDeclarationArgument { get { return TokenType.FunctionDeclarationArgument; } }
+        private Condition FunctionBody { get { return TokenType.FunctionBody; } }
+        private Condition WhileLoop { get { return TokenType.WhileLoop; } }
+        #endregion
 
         public SyntaxMatcher(IEnumerable<Token> sourceTokens)
         {
@@ -20,67 +136,67 @@ namespace Lexer
             ParseRule[] AllRules = 
             {
                 #region Syntax rules
-                new ParseRule(TokenType.StatementNode,                    
-                    new Condition[]{TokenType.RValue, TokenType.EndOfLine},
-                    new Condition[]{TokenType.DeclarationNode},
-                    new Condition[]{TokenType.AssignmentNode},
-                    new Condition[]{TokenType.CodeBlockNode},
-                    new Condition[]{TokenType.WhileLoop}),
+                new ParseRule(StatementNode,                    
+                    RValue + EndOfLine,
+                    DeclarationNode,
+                    AssignmentNode,
+                    CodeBlockNode,
+                    WhileLoop),
             
-                new ParseRule(TokenType.DeclarationNode,
-                    new Condition[]{new Condition(TokenType.VariableModifier, ConditionType.OneOrMore), TokenType.Type, TokenType.Symbol, TokenType.EndOfLine},
-                    new Condition[]{TokenType.Type, TokenType.Symbol, TokenType.EndOfLine}),
+                new ParseRule(DeclarationNode,
+                    OneOrMore(VariableModifier) + Type + Symbol + EndOfLine,
+                    Type + Symbol + EndOfLine),
             
-                new ParseRule(TokenType.VariableModifier, 
-                    new Condition[]{TokenType.Const},
-                    new Condition[]{TokenType.Internal},
-                    new Condition[]{TokenType.Private},
-                    new Condition[]{TokenType.Public},
-                    new Condition[]{TokenType.Protected},
-                    new Condition[]{TokenType.Static},
-                    new Condition[]{TokenType.Virtual}),
+                new ParseRule(VariableModifier, 
+                    Const,
+                    Internal,
+                    Private,
+                    Public,
+                    Protected,
+                    Static,
+                    Virtual),
 
-                new ParseRule(TokenType.AssignmentNode,
-                    new Condition[]{TokenType.LValue, TokenType.Assignment, TokenType.Value, TokenType.EndOfLine}),
+                new ParseRule(AssignmentNode,
+                    LValue + Assignment + Value + EndOfLine),
             
-                new ParseRule(TokenType.CodeBlockNode,
-                    new Condition[]{TokenType.LeftCurlyBracket, new Condition(TokenType.StatementNode, ConditionType.OneOrMore), TokenType.RightCurlyBracket},
-                    new Condition[]{TokenType.LeftCurlyBracket, TokenType.RightCurlyBracket}),
+                new ParseRule(CodeBlockNode,
+                    LeftCurlyBracket + OneOrMore(StatementNode) + RightCurlyBracket,
+                    LeftCurlyBracket + RightCurlyBracket),
 
-                new ParseRule(TokenType.Value,                    
-                    new Condition[]{TokenType.RValue},
-                    new Condition[]{TokenType.LValue}),
+                new ParseRule(Value,                    
+                    RValue,
+                    LValue),
 
-                new ParseRule(TokenType.LValue,
-                    new Condition[]{TokenType.FullSymbol}),
+                new ParseRule(LValue,
+                    FullSymbol),
  
-                new ParseRule(TokenType.RValue,                    
-                    new Condition[]{TokenType.FunctionCall},
-                    new Condition[]{TokenType.FullSymbol}),
+                new ParseRule(RValue,                    
+                    FunctionCall,
+                    FullSymbol),
 
-                new ParseRule(TokenType.FunctionCall,
-                    new Condition[]{TokenType.FullSymbol, TokenType.LeftBracket, TokenType.RightBracket},
-                    new Condition[]{TokenType.FullSymbol, TokenType.LeftBracket, TokenType.Value, new Condition(TokenType.FunctionArgument, ConditionType.OneOrMore), TokenType.RightBracket},
-                    new Condition[]{TokenType.FullSymbol, TokenType.LeftBracket, TokenType.Value, TokenType.RightBracket}),
+                new ParseRule(FunctionCall,
+                    FullSymbol + LeftBracket + RightBracket,
+                    FullSymbol + LeftBracket + Value + OneOrMore(FunctionArgument) + RightBracket,
+                    FullSymbol + LeftBracket + Value + RightBracket),
 
-                new ParseRule(TokenType.FunctionArgument,
-                    new Condition[]{TokenType.Comma, TokenType.Value}),
+                new ParseRule(FunctionArgument,
+                    Comma + Value),
 
-                new ParseRule(TokenType.FullSymbol,
-                    new Condition[]{TokenType.Symbol, new Condition(TokenType.SubSymbol, ConditionType.OneOrMore)},
-                    new Condition[]{TokenType.Symbol}),
+                new ParseRule(FullSymbol,
+                    Symbol + OneOrMore(SubSymbol),
+                    Symbol),
 
-                new ParseRule(TokenType.SubSymbol,
-                    new Condition[]{TokenType.Period, TokenType.Symbol}),
+                new ParseRule(SubSymbol,
+                    Period + Symbol),
 
-                new ParseRule(TokenType.Type,
-                    new Condition[]{TokenType.FullSymbol, TokenType.LeftBracket, new Condition(TokenType.Type, ConditionType.OneOrMore), TokenType.RightBracket},
-                    new Condition[]{TokenType.FullSymbol, TokenType.LeftBracket, TokenType.RightBracket},
-                    new Condition[]{TokenType.FullSymbol}),
+                new ParseRule(Type,
+                    FullSymbol + LeftBracket + OneOrMore(Type) + RightBracket,
+                    FullSymbol + LeftBracket + RightBracket,
+                    FullSymbol),
 
 
-                new ParseRule(TokenType.WhileLoop,
-                    new Condition[]{TokenType.While, TokenType.LeftBracket, TokenType.Value, TokenType.RightBracket, TokenType.StatementNode})
+                new ParseRule(WhileLoop,
+                    While + LeftBracket + Value + RightBracket + StatementNode)
                 #endregion
             };
 
@@ -209,5 +325,10 @@ namespace Lexer
             }
             return new Tuple<AstNode, int>(node, tokensConsumed);
         } 
+
+        private Condition OneOrMore(Condition c)
+        {
+            return new Condition(c, ConditionType.OneOrMore);
+        }
     }
 }
