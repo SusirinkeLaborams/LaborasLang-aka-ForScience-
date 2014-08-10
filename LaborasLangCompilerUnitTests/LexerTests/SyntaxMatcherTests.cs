f95ddccc9b7ab3a10617d0dc9882b71d240869eb
AndriusDap <Dapsevicius.Andrius@gmail.com>
2014-08-10 19:25:29 +0300

    removed huge test file

--- LaborasLangCompilerUnitTests/LexerTests/SyntaxMatcherTests.cs
+++ LaborasLangCompilerUnitTests/LexerTests/SyntaxMatcherTests.cs
@@ -1,4 +1,4 @@
-﻿#define REMATCH
+﻿// #define REMATCH
 
 using System;
 using System.Linq;
@@ -397,60 +397,6 @@ auto bottles = string(int count)
 	return ret;
 };
 ";
-            ExecuteTest(source);
-        }
-
-        [TestMethod, TestCategory("Lexer"), TestCategory("SyntaxMatcher"), Timeout(timeout)]
-        public void TestCodeSampleLotsOfBottles()
-        {
-            var source = @"
-auto Main = int()
-{
-	auto count = 99;
-	System.Console.WriteLine(""{0} on the wall, {0}."", bottles(count));
-
-	while ((count -= 1) >= 0)
-	{
-		System.Console.WriteLine(""Take one down and pass it around, {0} on the wall."", bottles(count));
-		System.Console.WriteLine();
-		System.Console.WriteLine(""{0} on the wall, {0}."", bottles(count));
-	
-	}
-	System.Console.WriteLine(""Go to the store and buy some more, {0} on the wall."", bottles(99));
-	System.Console.ReadKey();
-	return 0;
-};
-
-auto bottles = string(int count)
-{
-	string ret;
-
-	if (count > 0)
-	{
-		ret += count;
-
-		if (count > 1)
-		{
-			ret += "" bottles "";
-		}
-		else
-		{
-			ret += "" bottle "";
-		}
-	}
-	else
-	{
-		ret += ""no more bottles "";
-	}
-
-	ret += ""of beer"";
-	return ret;
-};
-";
-            for (int i = 0; i < 10; i++)
-            {
-                source += source;
-            };
             ExecuteTest(source);
         }
 
