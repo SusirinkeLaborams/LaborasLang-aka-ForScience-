7c1c5642cfa98eddcfc3dbfbe0cbaaae1dfd7e92
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2014-09-02 09:39:46 +0300

    Fix FastString == operator

--- Lexer/Containers/FastString.cs
+++ Lexer/Containers/FastString.cs
@@ -56,6 +56,9 @@ namespace Lexer.Containers
                 {
                     return false;
                 }
+                
+                aBuffer++;
+                bBuffer++;
             }
 
             return true;
