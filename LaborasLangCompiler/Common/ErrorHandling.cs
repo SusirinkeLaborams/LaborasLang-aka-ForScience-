6859f4041b5fba63a67a21fc249f8055e0302d87
DariusL <d.lapunas@gmail.com>
2015-02-06 17:44:17 +0200

    Added ErrorHandling

---
+++ LaborasLangCompiler/Common/ErrorHandling.cs
@@ -0,0 +1,65 @@
+﻿using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaborasLangCompiler.Common
+{
+    public enum ErrorCode
+    {
+    }
+
+    public static class ErrorHandling
+    {
+        public static IReadOnlyList<Error> Errors { get { return errors; } }
+
+        private static List<Error> errors = new List<Error>();
+
+        public static void Report(ErrorCode error, SequencePoint point, string message)
+        {
+            if (message == null)
+                throw new ArgumentNullException("Message must not be null");
+            var newError = new Error(point, error, message);
+            errors.Add(newError);
+
+            Console.WriteLine(newError);
+        }
+
+        public static void Report(ErrorCode error, string message)
+        {
+            Report(error, null, message);
+        }
+
+        public static void Clear()
+        {
+            errors = new List<Error>();
+        }
+
+        public class Error
+        {
+            public SequencePoint Point { get; private set; }
+            public ErrorCode ErrorCode { get; private set; }
+            public string Message { get; private set; }
+
+            public Error(SequencePoint point, ErrorCode error, string message)
+            {
+                Point = point;
+                ErrorCode = error;
+                Message = message;
+            }
+
+            public override string ToString()
+            {
+                StringBuilder builder = new StringBuilder();
+                if(Point != null)
+                {
+                    builder.Append(Point).Append(": ");
+                }
+                builder.AppendFormat("error {0:4}: {2}", ErrorCode, Message);
+                return builder.ToString();
+            }
+        }
+    }
+}
