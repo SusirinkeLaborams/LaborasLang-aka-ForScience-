a5bf0b8964fb9db44aaa90069633596a98a22bf4
DariusL <d.lapunas@gmail.com>
2015-02-16 20:40:57 +0200

    Implemented IInitializerList

---
+++ LaborasLangCompiler/Parser/Impl/InitializerList.cs
@@ -0,0 +1,48 @@
+﻿using LaborasLangCompiler.Common;
+using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaborasLangCompiler.Parser.Utils;
+
+namespace LaborasLangCompiler.Parser.Impl
+{
+    class InitializerList : ParserNode, IInitializerList
+    {
+        public override NodeType Type { get { return NodeType.InitializerList; } }
+
+        public IReadOnlyList<IExpressionNode> Initializers { get { return expressions; } }
+
+        private IReadOnlyList<ExpressionNode> expressions;
+        private InitializerList(IReadOnlyList<ExpressionNode> expressions, SequencePoint point) : base(point)
+        {
+            this.expressions = expressions;
+        }
+
+        public static InitializerList Create(ContextNode context, IEnumerable<ExpressionNode> expressions, SequencePoint point)
+        {
+            foreach(var exp in expressions)
+            {
+                if(!exp.IsGettable)
+                {
+                    ErrorCode.NotAnRValue.ReportAndThrow(exp.SequencePoint, "Initializer list items must be gettable");
+                }
+            }
+
+            return new InitializerList(expressions.ToArray(), point);
+        }
+
+        public override string ToString(int indent)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Indent(indent).AppendLine("InitializerList:");
+            foreach(var exp in expressions)
+            {
+                builder.AppendLine(exp.ToString(indent + 2));
+            }
+            return builder.ToString();
+        }
+    }
+}
