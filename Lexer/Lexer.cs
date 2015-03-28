bffe054351101b060cf9b462189537942c3a2b1e
AndriusDap <Dapsevicius.Andrius@gmail.com>
2015-03-28 13:44:18 +0200

    Modified exposed lexer interface

--- Lexer/Lexer.cs
+++ Lexer/Lexer.cs
@@ -1,5 +1,8 @@
 ﻿using Lexer.Containers;
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace Lexer
 {
@@ -19,9 +22,39 @@ namespace Lexer
         {
             using (var rootNode = new RootNode())
             {
-                var tokens = Tokenizer.Tokenize(source, rootNode);
-                var matcher = new SyntaxMatcher(tokens, rootNode);
-                consumer.Invoke(matcher.Match());
+                consumer.Invoke(AstNodeExtractor.Invoke(rootNode, source));
+            }
+        }
+
+        public static void WithTree(IReadOnlyList<string> sources, Action<IReadOnlyList<AstNode>> consumer)
+        {
+            var roots = new List<RootNode>();
+            try
+            {
+                roots.AddRange(sources.Select(source => new RootNode()));
+
+                var nodes = roots.Zip(sources, AstNodeExtractor).ToArray();
+                consumer.Invoke(nodes);
+            }
+            finally
+            {
+                foreach (var rootNode in roots)
+                {
+                    rootNode.Dispose();
+                }
+            }
+        }
+
+        private static Func<RootNode, string, AstNode> AstNodeExtractor
+        {
+            get
+            {
+                return (root, source) =>
+                {
+                    var sourceTokens = Tokenizer.Tokenize(source, root);
+                    var syntaxMatcher = new SyntaxMatcher(sourceTokens, root);
+                    return syntaxMatcher.Match();
+                };
             }
         }
     }
