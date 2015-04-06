4ec0ab9639b4eb13f91534b7f818a17584403ebf
AndriusDap <dapsevicius.andrius@gmail.com>
2015-04-06 21:58:29 +0300

    making full symbol recursive

---
+++ Lexer/PostProcessors/FullSymbolPostProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexer.PostProcessors
+{
+    class FullSymbolPostProcessor : PostProcessor
+    {
+        public override void Transform(AbstractSyntaxTree astNode)
+        {
+            if (astNode.Type == TokenType.FullSymbol)
+            {
+                if (astNode.Children.Count == 1)
+                {
+                    return;
+                }
+                else
+                {
+                    var symbol = astNode.Children.First();
+                    
+                    var source = astNode.Children;
+                    var remainder = new AbstractSyntaxTree(new Node(TokenType.FullSymbol), source.GetRange(2, source.Count - 2));
+                    
+                    astNode.Children = new List<AbstractSyntaxTree>() { symbol, remainder };
+                }
+            }
+        }
+    }
+}
