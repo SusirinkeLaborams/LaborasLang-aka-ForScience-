df240ebcf7c5ba066673ce205fe1f10b616576b8
DariusL <d.lapunas@gmail.com>
2015-04-22 23:26:42 +0300

    Fixing negative literals in lexer

---
+++ Lexer/PostProcessors/NegativeNumberResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexer.PostProcessors
+{
+    class NegativeNumberResolver : PostProcessor
+    {
+        public override void Transform(AbstractSyntaxTree astNode)
+        {
+            if(astNode.Type == TokenType.PrefixNode && astNode.Children[1].Type == TokenType.Minus)
+            {
+
+                switch (astNode.Children[0].Type)
+                {
+                    case TokenType.Float:
+                    case TokenType.Integer:
+                    case TokenType.Double:
+                    case TokenType.Long:
+                        astNode.ReplaceWith(astNode.Children[0]);
+                        astNode.Content = "-" + astNode.Content;
+                        astNode.Node.Start = new Location(astNode.Node.Start.Column - 1, astNode.Node.Start.Row);
+                        break;
+                }
+            }
+        }
+    }
+}
